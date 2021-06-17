932b127fd0b4b0baed9473ee911a16f458225d1b
democvidev <cnvvc@mail.ru>
2021-06-17 15:24:52 +0200

    configurated flash messages

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-
+            $this->addFlash('success', 'A confirmation email has been sent to your inbox. Please confirm your new acconut.');
             return $this->redirectToRoute('home');
         }
 
